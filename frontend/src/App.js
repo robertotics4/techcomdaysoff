a3be1279f0792150e55aead6490947637a8ed1d2
Roberto Oliveira <robertooliveiragp@gmail.com>
2019-10-10 19:59:34 -0300

    frontend: iniciando o frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={logo} alt="Techcom"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
