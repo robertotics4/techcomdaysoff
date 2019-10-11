6f0e75614fc0e7b6aef1e117394b63b1404bdfc6
robertotics4 <robertooliveiragp@gmail.com>
2019-10-11 12:01:45 -0300

    frontend: implementando dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+import './styles.css';
 
 export default function Dashboard() {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    async function loadBookings() {
+      const response = await api.get('bookings');
+      setBookings(response.data);
+    }
+
+    loadBookings();
+  }, []);
+
   return (
     <>
-      <p>Programe suas <strong>folgas</strong> e organize sua agenda.</p>
-      <button
-        className="btn"
-        type="button"
-        onClick={() => {}}
-      >Marcar folga</button>
+      <ul className="booking-list">
+        {bookings.map(booking => (
+          <li key={booking._id}>
+            <header>
+              {booking.date}
+            </header>
+            <span>Solicitada em {booking.createdAt}</span>
+            <span>Turno: {booking.officeHour}</span>
+            <strong>{booking.user.nome}</strong>
+            <span>{booking.user.folgas} folgas disponíveis</span>
+            <span>{booking.user.email}</span>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
+
+
+/*
+<p>Programe suas <strong>folgas</strong> e organize sua agenda.</p>
+      <button
+        className="btn"
+        type="button"
+        onClick={() => { }}
+      >Marcar folga</button>
+*/
